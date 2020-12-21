8ec77a251faa1f401918ad39b9f2102d93f38f50
Rebekah <rebekahjanedejong@gmail.com>
2020-12-21 18:08:33 -0500

    Add hamburger menu to left at 800px breakpoint

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 import { HeaderWrap } from "../../Elements/HeaderWrap/";
 
 function Header({text}) {
     return (
         <HeaderWrap>
+            <FontAwesomeIcon className="menu" icon={faBars} size="lg" />
             <h1>{text}</h1>
         </HeaderWrap> 
     )
--- src/Elements/HeaderWrap/index.js
+++ src/Elements/HeaderWrap/index.js
@@ -1,19 +1,44 @@
 import styled from 'styled-components';
 
+const media = { desktop: "@media(min-width: 800px)" };
+
 export const HeaderWrap = styled.div`
     background-color: #6B1FB6;
+    display: flex;
+    align-items: center;
     padding: 30px 0;
     z-index: 3000;
     position: fixed;
     top: 0;
     width: 100%;
+    height: 50px;
+    color: white;
 
-    h1 {
-        margin: 0;
-        font-family: 'Adamina', serif;
-        font-size: 36px;
-        color: #FFF;
-        text-align: center;
+    .menu {
+        display: block;
+        padding-left: 24px;
 
     }
+
+    h1 {
+        margin: 0 auto;
+        font-family: 'Adamina', serif;
+        font-size: 24px;
+        padding-right: 24px;
+    }
+
+    ${media.desktop} {
+        display: block;
+
+        .menu {
+            display: none;
+        }
+
+        h1 {
+            text-align: center;
+            font-size: 36px;
+        }
+    }
+    
+    
 `
