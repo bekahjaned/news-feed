acf02a7518f4778a4af35566165c3cd559ea1ef5
Rebekah <rebekahjanedejong@gmail.com>
2021-08-11 20:49:30 -0400

    Update article link text

--- src/Components/NewsText/index.js
+++ src/Components/NewsText/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import { NewsTextWrap } from '../../Elements/NewsTextWrap/'
 
-function NewsText({title, description, url}) {
+function NewsText({ title, description, url }) {
     return (
         <NewsTextWrap>
             <h3>{title}</h3>
             <p>{description}</p>
-            <a href={url} target="_blank" rel="noreferrer">Read more</a>
+            <a href={url} target="_blank" rel="noreferrer">Go to article</a>
         </NewsTextWrap>
     );
 };
