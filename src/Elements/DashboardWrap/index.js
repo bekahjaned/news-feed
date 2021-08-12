5c8df601dd86b691d1e333319b863707d0f33e00
Rebekah <rebekahjanedejong@gmail.com>
2021-08-11 20:59:01 -0400

    Improve accesibility

--- src/Elements/DashboardWrap/index.js
+++ src/Elements/DashboardWrap/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const DashboardWrap = styled.div`
+export const DashboardWrap = styled.main`
     display: flex;
     flex-direction: column;
     margin-top: 109px;
